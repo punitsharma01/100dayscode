0dae349839a277c20d40c07cbdb219455ecfc0f9
Punit Sharma <punitcs0067@gmail.com>
2025-10-01 14:49:10 +0530

    Added functionality to snake not move backward

--- day20/snake.py
+++ day20/snake.py
@@ -1,10 +1,12 @@
 from turtle import Turtle
 MOVE_DISTANCE = 20
+RIGHT, LEFT, UP, DOWN = 0, 180, 90, 270
 
 class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
         for i in range(0, 3):
@@ -19,16 +21,20 @@ class Snake:
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        self.segments[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
 
     def turn_left(self):
-        self.segments[0].setheading(180)
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
 
     def turn_right(self):
-        self.segments[0].setheading(0)
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
 
     def turn_up(self):
-        self.segments[0].setheading(90)
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
 
     def turn_down(self):
-        self.segments[0].setheading(270)
+        if self.head.heading() != UP:
+            self.segments[0].setheading(DOWN)
