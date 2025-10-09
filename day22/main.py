43edb9644660100f779e7460d34df5db2c43bfce
Punit Sharma <punitcs0067@gmail.com>
2025-10-09 16:38:26 +0530

    Initial setup of the pong ganme

---
+++ day22/main.py
@@ -0,0 +1,12 @@
+from turtle import Screen, Turtle
+
+SCREEN_WID, SCREEN_HIG = 800, 500
+screen = Screen()
+screen.setup(width=SCREEN_WID, height=SCREEN_HIG)
+screen.bgcolor("black")
+screen.title("The Pong Game")
+
+
+
+
+screen.exitonclick()
