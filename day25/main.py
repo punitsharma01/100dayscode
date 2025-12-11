6cca007f56f505d726ca6ed8184bc6c52b34fd1d
Punit Sharma <punitcs0067@gmail.com>
2025-12-11 09:47:05 +0530

    starting to work with pandas and csv data filÃe

---
+++ day25/main.py
@@ -0,0 +1,18 @@
+import csv
+# with open("weather_data.csv", mode="r") as csvfile:
+#     data = csvfile.readlines()
+#     for item in data:
+#         print(item)
+
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
+#     print(temperatures)
+
+import pandas
+data = pandas.read_csv("weather_data.csv")
+print(data)
+print(data["temp"])
