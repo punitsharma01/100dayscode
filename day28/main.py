91fe67a4d27935488fccecc14fd5eaff304718fa
Punit Sharma <punitcs0067@gmail.com>
2025-12-17 15:41:44 +0530

    Added reset functionality for pomodoro Completed git add .

--- day28/main.py
+++ day28/main.py
@@ -10,7 +10,17 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 REPS = 0
+TIMER = None
+
+
 # ---------------------------- TIMER RESET ------------------------------- # 
+def reset_timer():
+    window.after_cancel(TIMER)
+    timer_label.config(text="TIMER", bg=GREEN)
+    canvas.itemconfig(timer_text, text="00:00")
+    check_mark.config(text="")
+    global REPS
+    REPS = 0
 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
@@ -19,24 +29,29 @@ def start_timer():
     REPS += 1
     if REPS % 2 == 1:
         count_down(WORK_MIN * 60)
-        timer_label.config(bg=GREEN)
+        timer_label.config(text="Work", bg=GREEN)
     elif REPS == 8:
         count_down(LONG_BREAK_MIN * 60)
-        timer_label.config(bg=RED)
+        timer_label.config(text="Long Break", bg=RED)
     else:
         count_down(SHORT_BREAK_MIN * 60)
-        timer_label.config(bg=PINK)
+        timer_label.config(text="Short Break", bg=PINK)
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
+    global TIMER
     count_min = math.floor(count / 60)
     count_sec = count % 60
     if count_sec <= 9:
         count_sec = f"0{count_sec}"
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        TIMER = window.after(1000, count_down, count - 1)
     else:
+        if REPS % 2 == 1:
+            checks = math.ceil(REPS/2) * "✓"
+            check_mark.config(text=checks)
         start_timer()
 
 
@@ -46,7 +61,7 @@ window.title("Pomodoro")
 window.config(width=600, height=400)
 window.config(padx=40, pady=24, bg=YELLOW)
 # canvas
-canvas = Canvas(width=360, height=280, bg=PINK, highlightthickness=0)
+canvas = Canvas(width=360, height=280, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(180, 140, image=tomato_img)
 timer_text = canvas.create_text(180, 170, text="00:00", fill="white", font=(FONT_NAME, 24, "bold"))
@@ -58,13 +73,12 @@ timer_label.grid(row=0, column=1)
 start_button = Button(text="Start", fg="green", command=start_timer)
 start_button.config(font=(FONT_NAME, 24, "bold"))
 start_button.grid(row=2, column=0)
-reset_button = Button(text="Reset", fg="green")
+reset_button = Button(text="Reset", fg="green",  command=reset_timer)
 reset_button.config(font=(FONT_NAME, 24, "bold"))
 reset_button.grid(row=2, column=2)
 
-check_label = Label(text="✓", fg="green", bg=YELLOW)
-check_label.config(font=(FONT_NAME, 24, "bold"))
-check_label.grid(row=3, column=1)
-
+check_mark = Label(fg="green", bg=YELLOW)
+check_mark.config(font=(FONT_NAME, 24, "bold"))
+check_mark.grid(row=3, column=1)
 
 window.mainloop()
