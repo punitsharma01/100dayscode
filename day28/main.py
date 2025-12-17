1eac2fae8ed930a972fcae7f9142de8afbdd3087
Punit Sharma <punitcs0067@gmail.com>
2025-12-17 12:35:20 +0530

    Added countdown mechanism

--- day28/main.py
+++ day28/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -11,9 +12,22 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    count_down(5 * 60)
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec <= 9:
+        count_sec = f"0{count_sec}"
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -24,13 +38,13 @@ window.config(padx=40, pady=24, bg=YELLOW)
 canvas = Canvas(width=360, height=280, bg=PINK, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(180, 140, image=tomato_img)
-canvas.create_text(180, 170, text="00:00", fill="white", font=(FONT_NAME, 24, "bold"))
+timer_text = canvas.create_text(180, 170, text="00:00", fill="white", font=(FONT_NAME, 24, "bold"))
 canvas.grid(row=1, column=1)
 
 timer_label = Label(text="Timer", fg="green", bg=YELLOW)
 timer_label.config(font=(FONT_NAME, 24, "bold"))
 timer_label.grid(row=0, column=1)
-start_button = Button(text="Start", fg="green")
+start_button = Button(text="Start", fg="green", command=start_timer)
 start_button.config(font=(FONT_NAME, 24, "bold"))
 start_button.grid(row=2, column=0)
 reset_button = Button(text="Reset", fg="green")
