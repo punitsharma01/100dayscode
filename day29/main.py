9db1664d224cc404fb615809765d7181753d6097
Punit Sharma <punitcs0067@gmail.com>
2025-12-21 20:40:43 +0530

    Added UI changes form, to work for password,

--- day29/main.py
+++ day29/main.py
@@ -7,7 +7,8 @@ from tkinter import *
 window = Tk()
 window.title("K-Vault Password Manager")
 YELLOW = "#f7f5dd"
-FONT_NAME = "Arial"
+GREEN = "#9bdeac"
+FONT = ("Arial", 12, "bold")
 window.config(width=400, height=400)
 window.config(padx=40, pady=40)
 
@@ -15,8 +16,33 @@ window.config(padx=40, pady=40)
 canvas = Canvas(width=360, height=280, bg=YELLOW, highlightthickness=0)
 logo_image = PhotoImage(file="logo.png")
 canvas.create_image(180, 140, image=logo_image)
+canvas.grid(row=0, column=1)
 
-canvas.grid(row=1, column=1)
+website_label = Label(text="Website", font=FONT)
+website_label.grid(row=1, column=0)
+website_entry = Entry(width=36)
+website_entry.grid(row=1, column=1, columnspan=2)
+website_value = website_entry.get()
+
+email_label = Label(text="Email/Username", font=FONT)
+email_label.grid(row=2, column=0)
+email_entry = Entry(width=36)
+email_entry.grid(row=2, column=1, columnspan=2)
+email_value = email_entry.get()
+
+password_label = Label(text="Password", font=FONT)
+password_label.grid(row=3, column=0)
+password_entry = Entry(width=20)
+password_entry.grid(row=3, column=1)
+password_value = password_entry.get()
+
+generate_password_button = Button(text="Generate")
+generate_password_button.config(bg=YELLOW, font=FONT)
+generate_password_button.grid(row=3, column=2)
+
+add_button = Button(text="Add")
+add_button.config(width=36, bg=GREEN, font=FONT)
+add_button.grid(row=4, column=1, columnspan=2)
 
 
 window.mainloop()
