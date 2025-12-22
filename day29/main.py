a1abad643e29cc612692e18f00db0ff49928960d
Punit Sharma <punitcs0067@gmail.com>
2025-12-22 22:33:13 +0530

    added Save detail mechanism

--- day29/main.py
+++ day29/main.py
@@ -2,6 +2,14 @@ from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save_details():
+    website_value = website_entry.get()
+    email_value = email_entry.get()
+    password_value = password_entry.get()
+    with open("data.txt", mode="a") as file:
+        details = f"{website_value} | {email_value} | {password_value}"
+        file.write(details)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -22,25 +30,24 @@ website_label = Label(text="Website", font=FONT)
 website_label.grid(row=1, column=0)
 website_entry = Entry(width=36)
 website_entry.grid(row=1, column=1, columnspan=2)
-website_value = website_entry.get()
+website_entry.focus()
 
 email_label = Label(text="Email/Username", font=FONT)
 email_label.grid(row=2, column=0)
 email_entry = Entry(width=36)
 email_entry.grid(row=2, column=1, columnspan=2)
-email_value = email_entry.get()
+email_entry.insert(0, "myemail@example.com")
 
 password_label = Label(text="Password", font=FONT)
 password_label.grid(row=3, column=0)
 password_entry = Entry(width=20)
 password_entry.grid(row=3, column=1)
-password_value = password_entry.get()
 
 generate_password_button = Button(text="Generate")
 generate_password_button.config(bg=YELLOW, font=FONT)
 generate_password_button.grid(row=3, column=2)
 
-add_button = Button(text="Add")
+add_button = Button(text="Add", command=save_details)
 add_button.config(width=36, bg=GREEN, font=FONT)
 add_button.grid(row=4, column=1, columnspan=2)
 
