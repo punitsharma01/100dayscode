a63f6ed2f48882c8f8ce8d8a46ffdd787a4472c9
Punit Sharma <punitcs0067@gmail.com>
2025-12-26 16:35:15 +0530

    Improved Phonetic name generator Done!

--- day26/main.py
+++ day26/main.py
@@ -13,8 +13,8 @@ for (key, value) in student_dict.items():
 student_data_frame = pandas.DataFrame(student_dict)
 
 #Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    print(row.student, row.score)
+# for (index, row) in student_data_frame.iterrows():
+#     print(row.student, row.score)
     # print(row) comes as series so we can access using . column
     #Access row.student or row.score
 
@@ -28,13 +28,25 @@ phonetic_codes_dict = {row.letter: row.code for (index, row) in phonetic_codes_d
 # print(phonetic_codes_dict)
 
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-input_name = input("Please enter your name: ").upper()
+def generate_code():
+    input_name = input("Please enter your name: ").upper()
+    try:
+        if len(input_name) > 0:
+            phonetic_names = [phonetic_codes_dict[letter] for letter in input_name]
+            phonetic_letter_name = {letter: phonetic_codes_dict[letter] for letter in input_name}
+        else:
+            raise ValueError
+    except KeyError:
+        print("Sorry! only letters allowed! Try Again ")
+        generate_code()
+    except ValueError:
+        print("Sorry! Empty name not allowed! Try Again ")
+        generate_code()
+    else:
+        print(phonetic_names)
+        print(phonetic_letter_name)
 
-phonetic_names = [phonetic_codes_dict[letter] for letter in input_name]
-print(phonetic_names)
 
-phonetic_letter_name = {letter: phonetic_codes_dict[letter] for letter in input_name}
-print(phonetic_letter_name)
-
+generate_code()
 # phonetic_name = {letter: code for (letter, code) in phonetic_codes_dict.items() if letter in input_name}
 # print(phonetic_name)
