3309f3f7809e08a93955041af5cb3784ab9c8847
Punit Sharma <punitcs0067@gmail.com>
2025-12-27 22:54:16 +0530

    Added removing words mechanism, the words already known. Completed!!

--- day31/main.py
+++ day31/main.py
@@ -1,8 +1,6 @@
 import random
-import time
 from tkinter import *
 import pandas
-from random import choice
 BACKGROUND_COLOR = "#B1DDC6"
 YELLOW = "#f7f5dd"
 GREEN = "#9bdeac"
@@ -10,17 +8,24 @@ FONT_SMALL = ("Arial", 24, "italic")
 FONT_LARGE = ("Arial", 40, "bold")
 
 # ----------------------- Reading data  ----------------#
-dataframe = pandas.read_csv("./data/french_words.csv")
-french_words = dataframe.to_dict(orient="records")
-random_dict = {}
+current_word = {}
+words_to_learn = {}
+
+try:
+    dataframe = pandas.read_csv("./data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data_file = pandas.read_csv("./data/french_words.csv")
+    words_to_learn = original_data_file.to_dict(orient="records")
+else:
+    words_to_learn = dataframe.to_dict(orient="records")
 
 
 def next_word():
-    global random_dict, flip_timer
+    global current_word, flip_timer
     window.after_cancel(flip_timer)
-    random_dict = random.choice(french_words)
-    french = random_dict["French"]
-    english = random_dict["English"]
+    current_word = random.choice(words_to_learn)
+    french = current_word["French"]
+    english = current_word["English"]
     canvas.itemconfig(canvas_image, image=front_image)
     canvas.itemconfig(card_title, text="French", fill="black")
     canvas.itemconfig(card_word, text=f"{french}", fill="black")
@@ -30,8 +35,14 @@ def next_word():
 def flip_card():
     canvas.itemconfig(canvas_image, image=back_image)
     canvas.itemconfig(card_title, text="English", fill='#fff')
-    canvas.itemconfig(card_word, text=random_dict["English"],  fill='#fff')
+    canvas.itemconfig(card_word, text=current_word["English"],  fill='#fff')
 
+
+def is_known():
+    words_to_learn.remove(current_word)
+    to_learn = pandas.DataFrame(words_to_learn)
+    to_learn.to_csv("./data/words_to_learn.csv", index=False)
+    next_word()
 
 # ----------------------- UI Setup ----------------#
 window = Tk()
@@ -54,7 +65,7 @@ cross_image = PhotoImage(file="./images/wrong.png")
 cross_button = Button(image=cross_image, highlightthickness=0, command=next_word)
 cross_button.grid(row=1, column=0)
 right_image = PhotoImage(file="./images/right.png")
-tick_button = Button(image=right_image, highlightthickness=0, command=next_word)
+tick_button = Button(image=right_image, highlightthickness=0, command=is_known)
 tick_button.grid(row=1, column=1)
 next_word()
 
