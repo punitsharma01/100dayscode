8652ef07a4b040bddfbdc74cf0d51e1aa6cd9d5c
Punit Sharma <punitcs0067@gmail.com>
2025-12-27 17:04:53 +0530

    Added functionality to show word on tick or cross

--- day31/main.py
+++ day31/main.py
@@ -1,13 +1,29 @@
+import random
 from tkinter import *
+import pandas
+from random import choice
 BACKGROUND_COLOR = "#B1DDC6"
-
-# ----------------------- UI Setup ----------------#
-window = Tk()
-window.title("Flash Card Learning")
 YELLOW = "#f7f5dd"
 GREEN = "#9bdeac"
 FONT_SMALL = ("Arial", 24, "italic")
 FONT_LARGE = ("Arial", 40, "bold")
+
+# ----------------------- Reading data  ----------------#
+dataframe = pandas.read_csv("./data/french_words.csv")
+french_words = dataframe.to_dict(orient="records")
+
+
+def next_word():
+    random_dict = random.choice(french_words)
+    french = random_dict["French"]
+    english = random_dict["English"]
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_word, text=f"{french}")
+
+
+# ----------------------- UI Setup ----------------#
+window = Tk()
+window.title("Flash Card Learning")
 window.config(bg=BACKGROUND_COLOR)
 window.config(padx=50, pady=50)
 
@@ -17,14 +33,15 @@ front_image = PhotoImage(file="./images/card_front.png")
 canvas.create_image(412, 274, image=front_image)
 canvas.grid(row=0, column=0, columnspan=2)
 canvas.config(bg=BACKGROUND_COLOR)
-canvas.create_text(412, 150, text="English", font=FONT_SMALL)
-canvas.create_text(412, 360, text="Hello", font=FONT_LARGE)
+card_title = canvas.create_text(412, 150, text="", font=FONT_SMALL)
+card_word = canvas.create_text(412, 320, text="", font=FONT_LARGE)
 
 cross_image = PhotoImage(file="./images/wrong.png")
-cross_button = Button(image=cross_image, highlightthickness=0)
+cross_button = Button(image=cross_image, highlightthickness=0, command=next_word)
 cross_button.grid(row=1, column=0)
 right_image = PhotoImage(file="./images/right.png")
-tick_button = Button(image=right_image, highlightthickness=0)
+tick_button = Button(image=right_image, highlightthickness=0, command=next_word)
 tick_button.grid(row=1, column=1)
+next_word()
 
 window.mainloop()
