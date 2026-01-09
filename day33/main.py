c97a071fa00fc4d45e2bb0edbc89ca57e0be285e
Punit Sharma <punitcs0067@gmail.com>
2026-01-09 15:07:12 +0530

    some practice for sunrise sunset api data

--- day33/main.py
+++ day33/main.py
@@ -1,9 +1,25 @@
 import requests
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+# data = response.json()
+# print(data)
+# lat = data["iss_position"]["latitude"]
+# long = data["iss_position"]["longitude"]
+# print(lat, long)
+
+LAT = 8.537981
+LONG = -80.782127
+parameters = {
+    "lat": LAT,
+    "lng": LONG,
+    "formatted": 0
+}
+
+response = requests.get(f"https://api.sunrise-sunset.org/json", params=parameters, verify=False)
 response.raise_for_status()
-data = response.json()
-print(data)
-lat = data["iss_position"]["latitude"]
-long = data["iss_position"]["longitude"]
-print(lat, long)
+sunrise_data = response.json()
+sunrise = sunrise_data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = sunrise_data["results"]["sunset"].split("T")[1].split(":")[0]
+
+print(sunrise, sunset)
