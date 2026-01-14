6da39bab79e5db23271a09291fb597b4d59a0457
Punit Sharma <punitcs0067@gmail.com>
2026-01-14 13:47:59 +0530

    Added function to get next question

--- day34/ui.py
+++ day34/ui.py
@@ -1,8 +1,12 @@
 from tkinter import *
+from quiz_brain import QuizBrain
+
 THEME_COLOR = "#375362"
 WIDTH, HEIGHT = 400, 600
 class QuizInterface:
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(width=WIDTH, height=HEIGHT, padx=20, pady=20, bg=THEME_COLOR)
@@ -14,6 +18,7 @@ class QuizInterface:
         self.score_label.grid(row=0, column=1)
         self.canvas = Canvas(self.window, width=300, height=250, bg="white")
         self.question_text = self.canvas.create_text(150, 100,
+                            width=280,
                            text="Some text goes here",
                            font=("Arial", 20, "italic"),
                            fill=THEME_COLOR)
@@ -25,7 +30,10 @@ class QuizInterface:
         self.tick_image = PhotoImage(file="./images/true.png")
         true_button = Button(image=self.tick_image, highlightthickness=0, padx=20, pady=20)
         true_button.grid(row=2, column=1)
-
-
+        self.get_next_question()
 
         self.window.mainloop()
+
+    def get_next_question(self):
+        question_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=question_text)
