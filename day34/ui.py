1635816a7547dcf3dc4fe79cd3da94e5ef9d9e04
Punit Sharma <punitcs0067@gmail.com>
2026-01-14 14:42:32 +0530

    Added disable buttons, final done..

--- day34/ui.py
+++ day34/ui.py
@@ -25,28 +25,30 @@ class QuizInterface:
         self.canvas.grid(row=1, column=0, columnspan=2, pady=30)
 
         self.cross_image = PhotoImage(file="./images/false.png")
-        false_button = Button(image=self.cross_image, highlightthickness=0,
+        self.false_button = Button(image=self.cross_image, highlightthickness=0,
                               padx=20, pady=20, command=self.true_answer)
-        false_button.grid(row=2, column=0)
+        self.false_button.grid(row=2, column=0)
         self.tick_image = PhotoImage(file="./images/true.png")
-        true_button = Button(image=self.tick_image, highlightthickness=0,
+        self.true_button = Button(image=self.tick_image, highlightthickness=0,
                              padx=20, pady=20, command=self.false_answer)
-        true_button.grid(row=2, column=1)
+        self.true_button.grid(row=2, column=1)
         self.get_next_question()
 
         self.window.mainloop()
 
     def get_next_question(self):
+        self.canvas.config(bg="white")
         if self.quiz.still_has_questions():
-            self.canvas.config(bg="white")
             self.score_label.config(text=f"Score: {self.quiz.score}/{len(self.quiz.question_list)}")
             question_text = self.quiz.next_question()
             self.canvas.itemconfig(self.question_text, text=question_text)
         else:
-            self.canvas.configure(bg="white")
             self.canvas.itemconfig(self.question_text,
-                                   text=f"Thanks for completing the quiz. Your Score"
+                                   text=f"Thanks for completing the quiz. Your Score is"
                                         f" is {self.quiz.score}/{len(self.quiz.question_list)}")
+            self.false_button.config(state="disabled")
+            self.true_button.config(state="disabled")
+
     def true_answer(self):
         is_right = self.quiz.check_answer("True")
         self.give_feedback(is_right)
